94796713b95fdb4d3b2f596fb4b59ead70c9c092
Luat <nguyentriluat19498.com>
2023-05-07 23:48:55 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -27,6 +27,30 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
+let getUserName = (sender_psid) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v16.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
 let handleGetStarted = () => {
     return new Promise(async (resolve, reject) => {
         try {
