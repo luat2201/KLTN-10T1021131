8f883e5a8bed732e50f3c151a1d132c742754472
Luat <nguyentriluat19498.com>
2023-05-08 08:33:46 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,4 +1,4 @@
-import { response } from "express";
+import { response, response } from "express";
 import request from "request"
 require('dotenv').config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
@@ -27,34 +27,30 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
-let getUserName = (sender_psid) => {
-    // Construct the message body
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    }
-
+let getUserName = async (sender_psid) => {
+    let username = '';
     // Send the HTTP request to the Messenger Platform
-    request({
-        "uri": "https://graph.facebook.com/v16.0/me/messages",
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
         "qs": { "access_token": PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
+        "method": "GET",
     }, (err, res, body) => {
+        console.log(body)
         if (!err) {
-            console.log('message sent!')
+            let response = JSON.parse(res)
+            username = `${response.first_name} ${response.last_name}`
         } else {
             console.error("Unable to send message:" + err);
         }
     });
+    return username
 }
 
 let handleGetStarted = () => {
     return new Promise(async (resolve, reject) => {
         try {
-            response = { "text": "Xin chào mừng bạn đến với nhà hàng Luffy" }
+            let username = await getUserName(sender_psid)
+            response = { "text": `Xin chào mừng ${username} đến với nhà hàng Luffy` }
             await callSendAPI(sender_psid, response)
             resolve('done');
         } catch (e) {
